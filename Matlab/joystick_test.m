6ac0cf4301735f6a221e224c9b749cd70cf98ef8
TirthChauhan1304 <tpc1322004@gmail.com>
2024-07-24 12:59:40 +0530

    Hardware Updates

--- Matlab/joystick_test.m
+++ Matlab/joystick_test.m
@@ -3,9 +3,9 @@ joy = vrjoystick(1);
 
 while 1
     
-  b = zeros(12,1);
+  b = zeros(10,1);
   
-  for n = 1:12
+  for n = 1:10
       
   b(n) = button(joy,n);
   disp(b(n));
@@ -15,6 +15,6 @@ while 1
   d = read(joy);
   disp(d);
   
-  pause(0.25); 
+  pause(0.02); 
   
 end
